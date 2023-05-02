e0edc3744731495cb071f8881f21d6b3a4cda1c6
Ryan Nitz <ryan.nitz@coinbase.com>
2023-05-01 21:09:41 -0400

    Init

---
+++ describe_deposit_instructions.go
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2023-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prime
+
+import (
+	"context"
+	"fmt"
+)
+
+type DescribeWalletDepositInstructionsRequest struct {
+	PortfolioId string `json:"portfolio_id"`
+	Id          string `json:"wallet_id"`
+	Type        string `json:"deposit_type"`
+}
+
+type DescribeWalletDepositInstructionsResponse struct {
+	Crypto  *CryptoDepositInstructions `json:"crypto_instructions"`
+	Fiat    *FiatDepositInstructions   `json:"fiat_instructions"`
+	Request *DescribeWalletDepositInstructionsRequest
+}
+
+type CryptoDepositInstructions struct {
+	Id                string `json:"id"`
+	Name              string `json:"name"`
+	Type              string `json:"type"`
+	Address           string `json:"address"`
+	AccountIdentifier string `json:"account_identifier"`
+}
+
+type FiatDepositInstructions struct {
+	Id            string `json:"id"`
+	Name          string `json:"name"`
+	Type          string `json:"type"`
+	AccountNumber string `json:"account_number"`
+	RoutingNumber string `json:"routing_number"`
+	ReferenceCode string `json:"reference_code"`
+}
+
+func (c Client) DescribeWalletDepositInstructions(
+	ctx context.Context,
+	request *DescribeWalletDepositInstructionsRequest,
+) (*DescribeWalletDepositInstructionsResponse, error) {
+
+	path := fmt.Sprintf("/portfolios/%s/wallets/%s/deposit_instructions", request.PortfolioId, request.Id)
+
+	queryParams := fmt.Sprintf("?deposit_type=%s", request.Type)
+
+	response := &DescribeWalletDepositInstructionsResponse{Request: request}
+
+	if err := get(ctx, c, path, queryParams, request, response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
---
+++ test/describe_deposit_instructions_test.go
@@ -0,0 +1,68 @@
+package test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	prime "github.com/coinbase-samples/prime-sdk-go"
+)
+
+func TestDescribeWalletDepositInstructions(t *testing.T) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := newLiveTestClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	walletsResponse, err := client.DescribeWallets(ctx, &prime.DescribeWalletsRequest{
+		PortfolioId: client.Credentials.PortfolioId,
+		Type:        prime.WalletTypeVault,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if walletsResponse == nil {
+		t.Fatal(err)
+	}
+
+	if len(walletsResponse.Wallets) == 0 {
+		t.Fatal("expected crypto wallets in describe")
+	}
+
+	testDescribeDepositInstructions(t, client, walletsResponse.Wallets[0].Id)
+}
+
+func testDescribeDepositInstructions(t *testing.T, client *prime.Client, walletId string) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	response, err := client.DescribeWalletDepositInstructions(ctx, &prime.DescribeWalletDepositInstructionsRequest{
+		PortfolioId: client.Credentials.PortfolioId,
+		Id:          walletId,
+		Type:        prime.WalletDepositTypeCrypto,
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if response == nil {
+		t.Fatal("expected wallet deposit response to not be nil")
+	}
+
+	if response.Crypto == nil {
+		t.Fatal("expected crypto deposit instructions to not be nil")
+	}
+
+	if len(response.Crypto.Address) == 0 {
+		t.Fatal("expected crypto deposit address to be set")
+	}
+
+}
