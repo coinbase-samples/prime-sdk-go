c370b276ab166e2e66a8135f46db9d2d577b4004
Ryan Nitz <ryan.nitz@coinbase.com>
2023-05-02 18:48:27 -0400

    Init

---
+++ describe_assets.go
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2023-present Coinbase Global, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prime
+
+import (
+	"context"
+	"fmt"
+)
+
+type DescribeAssetsRequest struct {
+	EntityId string `json:"entity_id"`
+}
+
+type Asset struct {
+	Name             string `json:"name"`
+	Symbol           string `json:"symbol"`
+	DecimalPrecision string `json:"decial_precision"`
+	TradingSupported bool   `json:"trading_supported"`
+	ExplorerUrl      string `json:"explorer_url"`
+}
+
+type DescribeAssetsResponse struct {
+	Assets  []*Asset               `json:"assets"`
+	Request *DescribeAssetsRequest `json:"request"`
+}
+
+func (c Client) DescribeAssets(
+	ctx context.Context,
+	request *DescribeAssetsRequest,
+) (*DescribeAssetsResponse, error) {
+
+	path := fmt.Sprintf("/entities/%s/assets", request.EntityId)
+
+	response := &DescribeAssetsResponse{Request: request}
+
+	if err := get(ctx, c, path, emptyQueryParams, request, response); err != nil {
+		return response, fmt.Errorf("unable to DescribeAssets: %w", err)
+	}
+
+	return response, nil
+}
