c21efe1bf00309b26f466ead89d314169ab63be0
Carl Fluke <carl.fluke@coinbase.com>
2026-02-02 12:43:25 -0500

    pagination to positions and products

--- products/list_products.go
+++ products/list_products.go
@@ -32,9 +32,35 @@ type ListProductsRequest struct {
 }
 
 type ListProductsResponse struct {
-	Products   []*model.Product     `json:"products"`
-	Pagination *model.Pagination    `json:"pagination"`
-	Request    *ListProductsRequest `json:"-"`
+	model.PaginationMixin
+	Products      []*model.Product     `json:"products"`
+	Request       *ListProductsRequest `json:"-"`
+	service       ProductsService
+	serviceConfig *model.ServiceConfig
+}
+
+// Next fetches the next page of products using the pagination cursor.
+// Returns nil if there are no more pages.
+func (r *ListProductsResponse) Next(ctx context.Context) (*ListProductsResponse, error) {
+	if !r.HasNext() {
+		return nil, nil
+	}
+
+	nextRequest := *r.Request
+	nextRequest.Pagination = model.PrepareNextPagination(r.Request.Pagination, r.GetNextCursor())
+
+	return r.service.ListProducts(ctx, &nextRequest)
+}
+
+// Iterator returns a PageIterator for iterating through all pages of products.
+func (r *ListProductsResponse) Iterator() *model.PageIterator[*ListProductsResponse, *model.Product] {
+	return model.NewPageIteratorWithConfig(
+		r,
+		func(resp *ListProductsResponse) []*model.Product {
+			return resp.Products
+		},
+		r.serviceConfig,
+	)
 }
 
 func (s *productsServiceImpl) ListProducts(
@@ -44,9 +70,15 @@ func (s *productsServiceImpl) ListProducts(
 
 	path := fmt.Sprintf("/portfolios/%s/products", request.PortfolioId)
 
+	request.Pagination = utils.ApplyDefaultLimit(request.Pagination, s.serviceConfig)
+
 	queryParams := utils.AppendPaginationParams(core.EmptyQueryParams, request.Pagination)
 
-	response := &ListProductsResponse{Request: request}
+	response := &ListProductsResponse{
+		Request:       request,
+		service:       s,
+		serviceConfig: s.serviceConfig,
+	}
 
 	if err := core.HttpGet(
 		ctx,
